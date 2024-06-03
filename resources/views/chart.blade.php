9a03d3b943b0fd30478c04fb8a6d001ae87fc013
pavilion <jakisfajnyktos@gmail.com>
2024-06-03 18:30:11 +0200

    chart format

--- resources/views/chart.blade.php
+++ resources/views/chart.blade.php
@@ -1,20 +1,24 @@
 <?php
     use App\Http\Controllers\PricesController;
     use \App\Http\Controllers\StopyController;
+    $miasto = $_GET['city'];
+    $category = $_GET['category'];
+
+
 $dataPoints = array(
 );
 $dataPoints2 = array();
-$data2 = json_decode((new PricesController)->in_category('Lublin','Rynek pierwotny(tranzakcja)')->content());
-//echo $data2;
+$data2 = json_decode((new PricesController)->in_category($miasto,$category)->content());
+
 $data_rates = json_decode((new StopyController())->all());
 
 foreach ($data_rates as $item){
-    array_push($dataPoints2,array('label'=>$item->data,'y'=>$item->ref));
+    array_push($dataPoints2,array('x'=>strtotime($item->data)*1000,'y'=>$item->ref));
 }
 foreach ($data2 as $item) {
-    array_push($dataPoints,array('label'=>((new StopyController)->kwartalToData($item->kwartal)),'y'=>$item->cena));
+    array_push($dataPoints,array('x'=>strtotime((new StopyController)->kwartalToData($item->kwartal))*1000,'y'=>$item->cena));
 };
-
+//dd($dataPoints2)
 ?>
 
 <div>
@@ -27,7 +31,7 @@ foreach ($data2 as $item) {
                 exportEnabled: true,
                 theme: "light1", // "light1", "light2", "dark1", "dark2"
                 title:{
-                    text: "Simple Column Chart with Index Labels"
+                    text: <?php echo "'".$miasto." - ".$category."'"?>
                 },
                 axisY:{
                     includeZero: true,
@@ -48,6 +52,7 @@ foreach ($data2 as $item) {
                 data: [{
                     type: "line", //change type to bar, line, area, pie, etc
                     //indexLabel: "{y}", //Shows y value on all Data Points
+                    xValueType: "dateTime",
                     indexLabelFontColor: "#5A5757",
                     indexLabelPlacement: "outside",
                     dataPoints: <?php echo json_encode($dataPoints);  ?>
@@ -55,6 +60,7 @@ foreach ($data2 as $item) {
                     type: "line", //change type to bar, line, area, pie, etc
                     //indexLabel: "{y}", //Shows y value on all Data Points
                     axisYType: "secondary",
+                    xValueType: "dateTime",
                     indexLabelFontColor: "#5A5757",
                     indexLabelPlacement: "outside",
                     dataPoints: <?php echo json_encode($dataPoints2);  ?>
